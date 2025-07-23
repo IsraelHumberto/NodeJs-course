e6036a2f698f2a46b26f9c65d0f00f4035c9fad6
Israel Humberto <rael@DESKTOP-R1QKHF1.localdomain>
2025-07-23 18:30:39 -0300

    feat: reading and writting files asynchronously

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync('./txt/output.txt', textOut);
+// fs.writeFileSync('./txt/output.txt', textOut);
 
-console.log(textIn);
+// Assincronous file reading
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    if (err) return 
+
+    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => {
+        conseol.log(data2);
+
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written');
+            });
+        })
+    })
+});
